03261677e7963c98c07613eba47d5fdbb4bcdb0b
Luke Mulcahy <luke.mulcahy@gmail.com>
2024-11-29 17:05:50 -0800

    Initial Commit

---
+++ StairsPathsPhotosMap/StairsPathsPhotosMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StairsPathsPhotosMapApp.swift
+//  StairsPathsPhotosMap
+//
+//  Created by Luke Mulcahy on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct StairsPathsPhotosMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StairsPathsPhotosMapTests/StairsPathsPhotosMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  StairsPathsPhotosMapTests.swift
+//  StairsPathsPhotosMapTests
+//
+//  Created by Luke Mulcahy on 11/29/24.
+//
+
+import Testing
+@testable import StairsPathsPhotosMap
+
+struct StairsPathsPhotosMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
