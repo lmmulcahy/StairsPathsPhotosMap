b5cc902663e97dd3d2d47cde0933ef8465e9f115
Luke Mulcahy <luke.mulcahy@gmail.com>
2024-11-30 21:00:26 -0800

    Split map tabs into photo view and map editing view

---
+++ StairsPathsPhotosMap/MapEditView.swift
@@ -0,0 +1,18 @@
+//
+//  MapEditView.swift
+//  StairsPathsPhotosMap
+//
+//  Created by Luke Mulcahy on 11/30/24.
+//
+
+import SwiftUI
+
+struct MapEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapEditView()
+}
---
+++ StairsPathsPhotosMap/PhotoMapView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoMapView.swift
+//  StairsPathsPhotosMap
+//
+//  Created by Luke Mulcahy on 11/30/24.
+//
+
+import SwiftUI
+
+struct PhotoMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoMapView()
+}
